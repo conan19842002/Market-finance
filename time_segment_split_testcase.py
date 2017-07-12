2702788a16983ac721206b0599c0824ca21a2e79
An Nguyen <Nguyenan@babraham.ac.uk>
2017-07-12 09:56:36 +0100

    update exception tests

--- time_segment_split_testcase.py
+++ time_segment_split_testcase.py
@@ -12,14 +12,14 @@ class TestTimeSegment(unittest.TestCase):
         """
         start = datetime.strptime('2015-01-01', '%Y-%m-%d').date()
         end = start
-        self.assertRaises('ERROR: The end date cannot be the same as start time', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError,'ERROR: The end date cannot be the same as start time', functest.getTimeSegment(start,end))
     def test_end_ealier_than_start_time(self):
         """
         Test that if start and end dates are the same, then throw an exception error
         """
         start = datetime.strptime('2015-01-01', '%Y-%m-%d').date()
         end = datetime.strptime('2014-01-01', '%Y-%m-%d').date()
-        self.assertRaises('ERROR: The end date cannot be earlier than the start time', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError, 'ERROR: The end date cannot be earlier than the start time', functest.getTimeSegment(start,end))
     def test_empty_start_end_time(self):
         """
         Test that if start date or end date is empty (or None) then throw en exception error
@@ -27,19 +27,19 @@ class TestTimeSegment(unittest.TestCase):
         #start date is empty, end date is not empty
         start = ''
         end = datetime.strptime('2014-01-01', '%Y-%m-%d').date()
-        self.assertRaises('ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError,'ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
         # start date is not empty, end date is empty
         start = datetime.strptime('2014-01-01', '%Y-%m-%d').date()
         end = ''
-        self.assertRaises('ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError,'ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
         #start date is None, end date is not None
         start = None
         end = ''
-        self.assertRaises('ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError,'ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
         #end date is None, start date is not None
         start = ''
         end = None
-        self.assertRaises('ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
+        self.assertRaises(TypeError,'ERROR: The start date and end date cannot be empty', functest.getTimeSegment(start,end))
     def test_time_segment_split(self):
         """
         Test that if start and end dates are valid, then split the time period in month segments
